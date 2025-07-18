911bdabc539bf7f98c3866cbc99dff2081c14e84
fak7o <166671204+fak7o@users.noreply.github.com>
2025-07-18 02:12:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,500 +1,91 @@
-[
-  {
-    "nombre": "Química General I",
-    "semestre": 1,
-    "abre": [
-      "Química General II",
-      "Laboratorio de Química General"
-    ]
-  },
-  {
-    "nombre": "Técnicas de Laboratorio Químico",
-    "semestre": 1,
-    "abre": [
-      "Laboratorio de Química General"
-    ]
-  },
-  {
-    "nombre": "Mecánica",
-    "semestre": 1,
-    "abre": [
-      "Electromagnetismo"
-    ]
-  },
-  {
-    "nombre": "Introducción al Cálculo",
-    "semestre": 1,
-    "abre": [
-      "Cálculo Diferencial e Integral",
-      "Electromagnetismo"
-    ]
-  },
-  {
-    "nombre": "El Químico Farmacéutico y su Acción",
-    "semestre": 1,
-    "abre": [
-      "El Medicamento y su Evolución"
-    ]
-  },
-  {
-    "nombre": "Cursos de Formación General",
-    "semestre": 1,
-    "abre": [
-      "Ciclo Licenciatura"
-    ]
-  },
-  {
-    "nombre": "Inglés I",
-    "semestre": 1,
-    "abre": [
-      "Inglés II"
-    ]
-  },
-  {
-    "nombre": "Química General II",
-    "semestre": 2,
-    "abre": [
-      "Química Orgánica I",
-      "Química Analítica I",
-      "Laboratorio I de Química Orgánica"
-    ]
-  },
-  {
-    "nombre": "Laboratorio de Química General",
-    "semestre": 2,
-    "abre": [
-      "Química Analítica I",
-      "Laboratorio I de Química Orgánica"
-    ]
-  },
-  {
-    "nombre": "Electromagnetismo",
-    "semestre": 2,
-    "abre": [
-      "Fisiología Celular"
-    ]
-  },
-  {
-    "nombre": "Cálculo Diferencial e Integral",
-    "semestre": 2,
-    "abre": [
-      "Estadística y Análisis de Datos"
-    ]
-  },
-  {
-    "nombre": "Biología General",
-    "semestre": 2,
-    "abre": [
-      "Fisiología Celular"
-    ]
-  },
-  {
-    "nombre": "El Medicamento y su Evolución",
-    "semestre": 2,
-    "abre": [
-      "Investigación para las Ciencias Farmacéuticas"
-    ]
-  },
-  {
-    "nombre": "Inglés II",
-    "semestre": 2,
-    "abre": [
-      "Inglés III",
-      "Investigación para las Ciencias Farmacéuticas"
-    ]
-  },
-  {
-    "nombre": "Química Orgánica I",
-    "semestre": 3,
-    "abre": [
-      "Química Orgánica II",
-      "Fisicoquímica I"
-    ]
-  },
-  {
-    "nombre": "Química Analítica I",
-    "semestre": 3,
-    "abre": [
-      "Química Analítica II",
-      "Laboratorio de Análisis Químico"
-    ]
-  },
-  {
-    "nombre": "Laboratorio I de Química Orgánica",
-    "semestre": 3,
-    "abre": []
-  },
-  {
-    "nombre": "Estadística y Análisis de Datos",
-    "semestre": 3,
-    "abre": [
-      "Laboratorio de Análisis Químico",
-      "Estadística Farmacéutica"
-    ]
-  },
-  {
-    "nombre": "Fisiología Celular",
-    "semestre": 3,
-    "abre": [
-      "Fisiología de Sistemas"
-    ]
-  },
-  {
-    "nombre": "Investigación para las Ciencias Farmacéuticas",
-    "semestre": 3,
-    "abre": [
-      "Práctica Intermedia",
-      "Gestión de Calidad"
-    ]
-  },
-  {
-    "nombre": "Inglés III",
-    "semestre": 3,
-    "abre": [
-      "Inglés IV"
-    ]
-  },
-  {
-    "nombre": "Cursos de Formación General",
-    "semestre": 3,
-    "abre": [
-      "Ciclo Licenciatura"
-    ]
-  },
-  {
-    "nombre": "Química Orgánica II",
-    "semestre": 4,
-    "abre": [
-      "Botánica y Farmacognosia",
-      "Química de Heterocíclicos y Análisis Espectroscópico",
-      "Bioquímica"
-    ]
-  },
-  {
-    "nombre": "Laboratorio de Análisis Químico",
-    "semestre": 4,
-    "abre": [
-      "Laboratorio de Análisis Instrumental",
-      "Farmacología General"
-    ]
-  },
-  {
-    "nombre": "Química Analítica II",
-    "semestre": 4,
-    "abre": [
-      "Laboratorio de Análisis Instrumental",
-      "Botánica y Farmacognosia"
-    ]
-  },
-  {
-    "nombre": "Fisicoquímica I",
-    "semestre": 4,
-    "abre": [
-      "Bioquímica",
-      "Fisicoquímica Farmacéutica"
-    ]
-  },
-  {
-    "nombre": "Fisiología de Sistemas",
-    "semestre": 4,
-    "abre": [
-      "Farmacología General",
-      "Fisiopatología Molecular"
-    ]
-  },
-  {
-    "nombre": "Práctica Intermedia",
-    "semestre": 4,
-    "abre": [
-      "Legislación Farmacéutica"
-    ]
-  },
-  {
-    "nombre": "Inglés IV",
-    "semestre": 4,
-    "abre": [
-      "Ciclo Profesional"
-    ]
-  },
-  {
-    "nombre": "Laboratorio de Análisis Instrumental",
-    "semestre": 5,
-    "abre": [
-      "Análisis de Medicamentos, Doping y Drogas de Abuso",
-      "Bromatología"
-    ]
-  },
-  {
-    "nombre": "Botánica y Farmacognosia",
-    "semestre": 5,
-    "abre": []
-  },
-  {
-    "nombre": "Química de Heterocíclicos y Análisis Espectroscópico",
-    "semestre": 5,
-    "abre": [
-      "Farmacoquímica I"
-    ]
-  },
-  {
-    "nombre": "Bioquímica",
-    "semestre": 5,
-    "abre": [
-      "Microbiología",
-      "Fisiopatología Molecular"
-    ]
-  },
-  {
-    "nombre": "Farmacología General",
-    "semestre": 5,
-    "abre": [
-      "Farmacoquímica I",
-      "Farmacología de Sistemas I",
-      "Biofarmacia y Farmacocinética"
-    ]
-  },
-  {
-    "nombre": "Gestión de Calidad",
-    "semestre": 5,
-    "abre": [
-      "Legislación Farmacéutica"
-    ]
-  },
-  {
-    "nombre": "Microbiología",
-    "semestre": 6,
-    "abre": [
-      "Farmacología de Sistemas II"
-    ]
-  },
-  {
-    "nombre": "Farmacoquímica I",
-    "semestre": 6,
-    "abre": [
-      "Farmacoquímica II"
-    ]
-  },
-  {
-    "nombre": "Farmacología de Sistemas I",
-    "semestre": 6,
-    "abre": [
-      "Farmacología de Sistemas II"
-    ]
-  },
-  {
-    "nombre": "Fisiopatología Molecular",
-    "semestre": 6,
-    "abre": [
-      "Fisiopatología y Semiología",
-      "Biotecnología Farmacéutica"
-    ]
-  },
-  {
-    "nombre": "Legislación Farmacéutica",
-    "semestre": 6,
-    "abre": [
-      "Salud Pública",
-      "Tecnología Farmacéutica I"
-    ]
-  },
-  {
-    "nombre": "Fisicoquímica Farmacéutica",
-    "semestre": 6,
-    "abre": [
-      "Tecnología Farmacéutica I",
-      "Operaciones Unitarias para Q&F"
-    ]
-  },
-  {
-    "nombre": "Fisiopatología y Semiología",
-    "semestre": 7,
-    "abre": [
-      "Nutrición Clínica",
-      "Bioquímica Clínica"
-    ]
-  },
-  {
-    "nombre": "Farmacoquímica II",
-    "semestre": 7,
-    "abre": [
-      "Análisis de Medicamentos, Doping y Drogas de Abuso"
-    ]
-  },
-  {
-    "nombre": "Farmacología de Sistemas II",
-    "semestre": 7,
-    "abre": [
-      "Farmacología Clínica",
-      "Toxicología"
-    ]
-  },
-  {
-    "nombre": "Salud Pública",
-    "semestre": 7,
-    "abre": [
-      "Estadística Farmacéutica",
-      "Farmacia Asistencial",
-      "Economía en Salud y Marketing Farmacéutico"
-    ]
-  },
-  {
-    "nombre": "Tecnología Farmacéutica I",
-    "semestre": 7,
-    "abre": [
-      "Biofarmacia y Farmacocinética",
-      "Tecnología Farmacéutica II",
-      "Administración y Gestión Farmacéutica"
-    ]
-  },
-  {
-    "nombre": "Operaciones Unitarias para Q & F",
-    "semestre": 7,
-    "abre": [
-      "Tecnología Farmacéutica II"
-    ]
-  },
-  {
-    "nombre": "Análisis de Medicamentos, Doping y Drogas de Abuso",
-    "semestre": 8,
-    "abre": [
-      "XI Semestre"
-    ]
-  },
-  {
-    "nombre": "Nutrición Clínica",
-    "semestre": 8,
-    "abre": [
-      "Bromatología"
-    ]
-  },
-  {
-    "nombre": "Bioquímica Clínica",
-    "semestre": 8,
-    "abre": [
-      "Farmacología Clínica",
-      "Toxicología"
-    ]
-  },
-  {
-    "nombre": "Biofarmacia y Farmacocinética",
-    "semestre": 8,
-    "abre": [
-      "Farmacología Clínica"
-    ]
-  },
-  {
-    "nombre": "Tecnología Farmacéutica II",
-    "semestre": 8,
-    "abre": [
-      "Tecnología Cosmética",
-      "Biotecnología Farmacéutica"
-    ]
-  },
-  {
-    "nombre": "Administración y Gestión Farmacéutica",
-    "semestre": 8,
-    "abre": [
-      "Farmacia Asistencial",
-      "Práctica Profesional en Farmacia Comunitaria",
-      "Economía en Salud y Marketing Farmacéutico"
-    ]
-  },
-  {
-    "nombre": "Estadística Farmacéutica",
-    "semestre": 8,
-    "abre": [
-      "Innovación y Proyectos"
-    ]
-  },
-  {
-    "nombre": "Farmacología Clínica",
-    "semestre": 9,
-    "abre": [
-      "Farmacia Clínica",
-      "Práctica Profesional en Farmacia Comunitaria",
-      "Economía en Salud y Marketing Farmacéutico"
-    ]
-  },
-  {
-    "nombre": "Bromatología",
-    "semestre": 9,
-    "abre": [
-      "XI Semestre"
-    ]
-  },
-  {
-    "nombre": "Toxicología",
-    "semestre": 9,
-    "abre": [
-      "Práctica Profesional en Farmacia Comunitaria"
-    ]
-  },
-  {
-    "nombre": "Farmacia Asistencial",
-    "semestre": 9,
-    "abre": [
-      "XI Semestre"
-    ]
-  },
-  {
-    "nombre": "Tecnología Cosmética",
-    "semestre": 9,
-    "abre": [
-      "XI Semestre"
-    ]
-  },
-  {
-    "nombre": "Electivos Especializados",
-    "semestre": 9,
-    "abre": [
-      "XI Semestre"
-    ]
-  },
-  {
-    "nombre": "Farmacia Clínica",
-    "semestre": 10,
-    "abre": [
-      "XI Semestre"
-    ]
-  },
-  {
-    "nombre": "Práctica Profesional en Farmacia Comunitaria",
-    "semestre": 10,
-    "abre": [
-      "XI Semestre"
-    ]
-  },
-  {
-    "nombre": "Biotecnología Farmacéutica",
-    "semestre": 10,
-    "abre": [
-      "XI Semestre"
-    ]
-  },
-  {
-    "nombre": "Economía en Salud y Marketing Farmacéutico",
-    "semestre": 10,
-    "abre": [
-      "XI Semestre"
-    ]
-  },
-  {
-    "nombre": "Innovación y Proyectos",
-    "semestre": 10,
-    "abre": [
-      "XI Semestre"
-    ]
-  },
-  {
-    "nombre": "Electivos Especializados",
-    "semestre": 10,
-    "abre": [
-      "XI Semestre"
-    ]
-  },
-  {
-    "nombre": "Actividad Final de Titulación",
-    "semestre": 11,
-    "abre": []
-  }
-]
+const malla = [
+  // I SEMESTRE
+  { nombre: "Química General I", semestre: 1, abre: ["Química General II", "Laboratorio de Química General"] },
+  { nombre: "Técnicas de Laboratorio Químico", semestre: 1, abre: ["Laboratorio de Química General"] },
+  { nombre: "Mecánica", semestre: 1, abre: ["Electromagnetismo"] },
+  { nombre: "Introducción al Cálculo", semestre: 1, abre: ["Cálculo Diferencial e Integral", "Electromagnetismo"] },
+  { nombre: "El Químico Farmacéutico y su Acción", semestre: 1, abre: ["El Medicamento y su Evolución"] },
+  { nombre: "Cursos de Formación General", semestre: 1, abre: ["Ciclo Licenciatura"] },
+  { nombre: "Inglés I", semestre: 1, abre: ["Inglés II"] },
+
+  // II SEMESTRE
+  { nombre: "Química General II", semestre: 2, abre: ["Química Orgánica I", "Química Analítica I", "Laboratorio I de Química Orgánica"] },
+  { nombre: "Laboratorio de Química General", semestre: 2, abre: ["Química Analítica I", "Laboratorio I de Química Orgánica"] },
+  { nombre: "Electromagnetismo", semestre: 2, abre: ["Fisiología Celular"] },
+  { nombre: "Cálculo Diferencial e Integral", semestre: 2, abre: ["Estadística y Análisis de Datos"] },
+  { nombre: "Biología General", semestre: 2, abre: ["Fisiología Celular"] },
+  { nombre: "El Medicamento y su Evolución", semestre: 2, abre: ["Investigación para las Ciencias Farmacéuticas"] },
+  { nombre: "Inglés II", semestre: 2, abre: ["Inglés III", "Investigación para las Ciencias Farmacéuticas"] },
+
+  // III SEMESTRE
+  { nombre: "Química Orgánica I", semestre: 3, abre: ["Química Orgánica II", "Fisicoquímica I"] },
+  { nombre: "Química Analítica I", semestre: 3, abre: ["Química Analítica II", "Laboratorio de Análisis Químico"] },
+  { nombre: "Laboratorio I de Química Orgánica", semestre: 3, abre: [] },
+  { nombre: "Estadística y Análisis de Datos", semestre: 3, abre: ["Laboratorio de Análisis Químico", "Estadística Farmacéutica"] },
+  { nombre: "Fisiología Celular", semestre: 3, abre: ["Fisiología de Sistemas"] },
+  { nombre: "Investigación para las Ciencias Farmacéuticas", semestre: 3, abre: ["Práctica Intermedia", "Gestión de Calidad"] },
+  { nombre: "Inglés III", semestre: 3, abre: ["Inglés IV"] },
+  { nombre: "Cursos de Formación General", semestre: 3, abre: ["Ciclo Licenciatura"] },
+
+  // IV SEMESTRE
+  { nombre: "Química Orgánica II", semestre: 4, abre: ["Botánica y Farmacognosia", "Química de Heterocíclicos y Análisis Espectroscópico", "Bioquímica"] },
+  { nombre: "Laboratorio de Análisis Químico", semestre: 4, abre: ["Laboratorio de Análisis Instrumental", "Farmacología General"] },
+  { nombre: "Química Analítica II", semestre: 4, abre: ["Laboratorio de Análisis Instrumental", "Botánica y Farmacognosia"] },
+  { nombre: "Fisicoquímica I", semestre: 4, abre: ["Bioquímica", "Fisicoquímica Farmacéutica"] },
+  { nombre: "Fisiología de Sistemas", semestre: 4, abre: ["Farmacología General", "Fisiopatología Molecular"] },
+  { nombre: "Práctica Intermedia", semestre: 4, abre: ["Legislación Farmacéutica"] },
+  { nombre: "Inglés IV", semestre: 4, abre: ["Ciclo Profesional"] },
+
+  // V SEMESTRE
+  { nombre: "Laboratorio de Análisis Instrumental", semestre: 5, abre: ["Análisis de Medicamentos, Doping y Drogas de Abuso", "Bromatología"] },
+  { nombre: "Botánica y Farmacognosia", semestre: 5, abre: [] },
+  { nombre: "Química de Heterocíclicos y Análisis Espectroscópico", semestre: 5, abre: ["Farmacoquímica I"] },
+  { nombre: "Bioquímica", semestre: 5, abre: ["Microbiología", "Fisiopatología Molecular"] },
+  { nombre: "Farmacología General", semestre: 5, abre: ["Farmacoquímica I", "Farmacología de Sistemas I", "Biofarmacia y Farmacocinética"] },
+  { nombre: "Gestión de Calidad", semestre: 5, abre: ["Legislación Farmacéutica"] },
+
+  // VI SEMESTRE
+  { nombre: "Microbiología", semestre: 6, abre: ["Farmacología de Sistemas II"] },
+  { nombre: "Farmacoquímica I", semestre: 6, abre: ["Farmacoquímica II"] },
+  { nombre: "Farmacología de Sistemas I", semestre: 6, abre: ["Farmacología de Sistemas II"] },
+  { nombre: "Fisiopatología Molecular", semestre: 6, abre: ["Fisiopatología y Semiología", "Biotecnología Farmacéutica"] },
+  { nombre: "Legislación Farmacéutica", semestre: 6, abre: ["Salud Pública", "Tecnología Farmacéutica I"] },
+  { nombre: "Fisicoquímica Farmacéutica", semestre: 6, abre: ["Tecnología Farmacéutica I", "Operaciones Unitarias para Q&F"] },
+
+  // VII SEMESTRE
+  { nombre: "Fisiopatología y Semiología", semestre: 7, abre: ["Nutrición Clínica", "Bioquímica Clínica"] },
+  { nombre: "Farmacoquímica II", semestre: 7, abre: ["Análisis de Medicamentos, Doping y Drogas de Abuso"] },
+  { nombre: "Farmacología de Sistemas II", semestre: 7, abre: ["Farmacología Clínica", "Toxicología"] },
+  { nombre: "Salud Pública", semestre: 7, abre: ["Estadística Farmacéutica", "Farmacia Asistencial", "Economía en Salud y Marketing Farmacéutico"] },
+  { nombre: "Tecnología Farmacéutica I", semestre: 7, abre: ["Biofarmacia y Farmacocinética", "Tecnología Farmacéutica II", "Administración y Gestión Farmacéutica"] },
+  { nombre: "Operaciones Unitarias para Q & F", semestre: 7, abre: ["Tecnología Farmacéutica II"] },
+
+  // VIII SEMESTRE
+  { nombre: "Análisis de Medicamentos, Doping y Drogas de Abuso", semestre: 8, abre: ["XI Semestre"] },
+  { nombre: "Nutrición Clínica", semestre: 8, abre: ["Bromatología"] },
+  { nombre: "Bioquímica Clínica", semestre: 8, abre: ["Farmacología Clínica", "Toxicología"] },
+  { nombre: "Biofarmacia y Farmacocinética", semestre: 8, abre: ["Farmacología Clínica"] },
+  { nombre: "Tecnología Farmacéutica II", semestre: 8, abre: ["Tecnología Cosmética", "Biotecnología Farmacéutica"] },
+  { nombre: "Administración y Gestión Farmacéutica", semestre: 8, abre: ["Farmacia Asistencial", "Práctica Profesional en Farmacia Comunitaria", "Economía en Salud y Marketing Farmacéutico"] },
+  { nombre: "Estadística Farmacéutica", semestre: 8, abre: ["Innovación y Proyectos"] },
+
+  // IX SEMESTRE
+  { nombre: "Farmacología Clínica", semestre: 9, abre: ["Farmacia Clínica", "Práctica Profesional en Farmacia Comunitaria", "Economía en Salud y Marketing Farmacéutico"] },
+  { nombre: "Bromatología", semestre: 9, abre: ["XI Semestre"] },
+  { nombre: "Toxicología", semestre: 9, abre: ["Práctica Profesional en Farmacia Comunitaria"] },
+  { nombre: "Farmacia Asistencial", semestre: 9, abre: ["XI Semestre"] },
+  { nombre: "Tecnología Cosmética", semestre: 9, abre: ["XI Semestre"] },
+  { nombre: "Electivos Especializados", semestre: 9, abre: ["XI Semestre"] },
+
+  // X SEMESTRE
+  { nombre: "Farmacia Clínica", semestre: 10, abre: ["XI Semestre"] },
+  { nombre: "Práctica Profesional en Farmacia Comunitaria", semestre: 10, abre: ["XI Semestre"] },
+  { nombre: "Biotecnología Farmacéutica", semestre: 10, abre: ["XI Semestre"] },
+  { nombre: "Economía en Salud y Marketing Farmacéutico", semestre: 10, abre: ["XI Semestre"] },
+  { nombre: "Innovación y Proyectos", semestre: 10, abre: ["XI Semestre"] },
+  { nombre: "Electivos Especializados", semestre: 10, abre: ["XI Semestre"] },
+
+  // XI SEMESTRE
+  { nombre: "Actividad Final de Titulación", semestre: 11, abre: [] }
+];
+
